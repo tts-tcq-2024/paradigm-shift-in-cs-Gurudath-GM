8996ce3c1aba7990cc4844e0a3c03d483a8173bc
Gurudath G M <princegguruk@gmail.com>
2024-07-29 21:55:34 +0530

    Update and rename checker.cs to  Program.cs

---
+++ Program.cs
@@ -0,0 +1,11 @@
+namespace BatteryManagementSystem_
+{
+    internal class Program
+    {
+        static int Main()
+        {
+            BatteryManagementSystemTest.Execution();
+            return 0;
+        }
+    }
+}

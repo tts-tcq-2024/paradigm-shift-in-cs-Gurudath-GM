6a46ea58e99d2f9c6eedbcd0f2b3b7f75e9ebe42
Gurudath G M <princegguruk@gmail.com>
2024-07-31 00:00:21 +0530

    Update BatteryManagementSystem.cs

--- BatteryManagementSystem.cs
+++ BatteryManagementSystem.cs
@@ -5,45 +5,46 @@ namespace BatteryManagementSystem_
         static bool check = true;
         private static bool IsBatteryTemperatureOk(float temperature)
         {
+            check = true;
             float minTemperature = 5, maxTemperature = 45;
             if (temperature < minTemperature || temperature > maxTemperature)
             {
-                Console.WriteLine("Temperature is out of range!");
+                BatteryStatus.Print("Temperature is out of range!");
                 check =  false;
             }
-            BatteryStatus.Check(minTemperature, maxTemperature, temperature, "Temperature");
+            BatteryStatus.Check(minTemperature, maxTemperature, temperature, "TEMPERATURE");
             return check;
         }
 
         private static bool IsBatteryStateOfChargeOk(float stateOfCharge)
         {
+            check = true;
             float minStateOfCharge = 20, maxStateOfCharge = 80;
             if (stateOfCharge < minStateOfCharge || stateOfCharge > maxStateOfCharge)
             {
-                Console.WriteLine("State of Charge(SoC) is out of range!");
+                BatteryStatus.Print("State of Charge is out of range!");
                 check = false;
             }
-            BatteryStatus.Check(minStateOfCharge, maxStateOfCharge, stateOfCharge, "StateOfCharge");
+            BatteryStatus.Check(minStateOfCharge, maxStateOfCharge, stateOfCharge, "SoC");
             return check;
         }
 
         private static bool IsBatteryChargeRateOk(float chargeRate)
         {
+            check = true;
             float minChargeRate = 0.2f, maxChargeRate = 0.8f;
             if (chargeRate < minChargeRate || chargeRate > maxChargeRate)
             {
-                Console.WriteLine("Charge Rate is out of range!");
+                BatteryStatus.Print("Charge Rate is out of range!");
                 check = false;                                           
             }
-            BatteryStatus.Check(minChargeRate, maxChargeRate, chargeRate, "ChargeRate");
+            BatteryStatus.Check(minChargeRate, maxChargeRate, chargeRate, "CHARGE_RATE");
             return check;
         }
 
         public static bool BatteryIsOk(float temperature, float stateOfCharge, float chargeRate)
         {
-            return (IsBatteryTemperatureOk(temperature) 
-                & IsBatteryStateOfChargeOk(stateOfCharge) 
-                & IsBatteryChargeRateOk(chargeRate));
+            return (IsBatteryTemperatureOk(temperature) & IsBatteryStateOfChargeOk(stateOfCharge) & IsBatteryChargeRateOk(chargeRate));
         }
     }
 }

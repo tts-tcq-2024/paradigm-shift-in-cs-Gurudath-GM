3126dbea4e7a993e2b31311048e1029dab05fa29
Gurudath G M <princegguruk@gmail.com>
2024-07-31 00:01:21 +0530

    Update BMS_Util.cs

--- BMS_Util.cs
+++ BMS_Util.cs
@@ -7,21 +7,21 @@ namespace BatteryManagementSystem_
         public static void LowWarning(float minValue, float maxValue, float value, string batteryState)
         {
             if (value > minValue && value <= (minValue + (maxValue * tolerance)))
-                Console.WriteLine($"LOW_{batteryState}_WARNING");
+                BatteryStatus.Print($"LOW_{batteryState}_WARNING");
         }
 
         public static void HighWarning(float minValue, float maxValue, float value, string batteryState)
         {
             if (value > (maxValue - (maxValue * tolerance)) && value <= maxValue)
-                Console.WriteLine($"HIGH_{batteryState}_WARNING");
+                BatteryStatus.Print($"HIGH_{batteryState}_WARNING");
         }
 
         public static void LowBreach(float minValue, float maxValue, float value, string batteryState)
         {
             if (value >= (minValue - minValue) && value <= minValue)
             {
-                Console.WriteLine($"LOW_{batteryState}_BREACH");
-                Console.WriteLine($"LOW_{batteryState}_WARNING");
+                BatteryStatus.Print($"LOW_{batteryState}_WARNING");
+                BatteryStatus.Print($"LOW_{batteryState}_BREACH");
             }
         }
 
@@ -29,8 +29,8 @@ namespace BatteryManagementSystem_
         {
             if (value > maxValue && value <= (maxValue + minValue))
             {
-                Console.WriteLine($"HIGH_{batteryState}_BREACH");
-                Console.WriteLine($"HIGH_{batteryState}_WARNING");
+                BatteryStatus.Print($"HIGH_{batteryState}_WARNING");
+                BatteryStatus.Print($"HIGH_{batteryState}_BREACH");
             }
         }
     }

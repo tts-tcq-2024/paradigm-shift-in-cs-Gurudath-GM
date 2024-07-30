b4b80db2d071dba08289f7842953009fce05302d
Gurudath G M <princegguruk@gmail.com>
2024-07-30 19:54:03 +0530

    Update BatteryStatus.cs

--- BatteryStatus.cs
+++ BatteryStatus.cs
@@ -3,38 +3,24 @@ namespace BatteryManagementSystem_
 {
     internal class BatteryStatus
     {
-        private const float tolerance = 0.05f;
         public static void Check(float minValue, float maxValue, float value, string batteryState) 
         {
             CheckWarning(minValue, maxValue, value, batteryState); 
+            CheckBreach(minValue, maxValue, value, batteryState);
         }
 
         private static void CheckWarning(float minValue, float maxValue, float value, string batteryState)
         {
-            if (value > minValue && value <= (minValue + (maxValue * tolerance)))
-            {
-                Console.WriteLine($"LOW_{batteryState}_WARNING");
-            }
-            else if (value > (maxValue - (maxValue * tolerance)) && value <= maxValue)
-            {
-                Console.WriteLine($"HIGH_{batteryState}_WARNING");
-            }
-            CheckBreach(minValue, maxValue, value, batteryState);
+            BMS_Util.LowWarning(minValue, maxValue, value, batteryState);
+            BMS_Util.HighWarning(minValue, maxValue, value, batteryState);
         }
 
         private static void CheckBreach(float minValue, float maxValue, float value, string batteryState)
         {
-            if(value >= (minValue - minValue) && value  <= minValue)
-            {
 
-                Console.WriteLine($"LOW_{batteryState}_BREACH");
-                Console.WriteLine($"LOW_{batteryState}_WARNING");
-            }
-            else if(value > maxValue && value <= (maxValue + minValue))
-            {
-                Console.WriteLine($"HIGH_{batteryState}_BREACH");
-                Console.WriteLine($"HIGH_{batteryState}_WARNING");
-            }
+            BMS_Util.LowBreach(minValue, maxValue, value, batteryState);
+            BMS_Util.HighBreach(minValue, maxValue, value, batteryState);
         }
+
     }
 }

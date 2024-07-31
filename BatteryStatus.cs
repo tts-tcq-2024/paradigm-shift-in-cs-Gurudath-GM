0cba44f409ef9a3429f5fa0b456403348064512b
Gurudath G M <princegguruk@gmail.com>
2024-07-31 18:43:33 +0530

    Update BatteryStatus.cs

--- BatteryStatus.cs
+++ BatteryStatus.cs
@@ -4,17 +4,17 @@ namespace paradigm_shift_csharp
     {
         public static void Check(float minValue, float maxValue, float value, string batteryState) 
         {
-            CheckWarning(minValue, maxValue, value, batteryState); 
-            CheckBreach(minValue, maxValue, value, batteryState);
+            Warning(minValue, maxValue, value, batteryState); 
+            Breach(minValue, maxValue, value, batteryState);
         }
 
-        private static void CheckWarning(float minValue, float maxValue, float value, string batteryState)
+        private static void Warning(float minValue, float maxValue, float value, string batteryState)
         {
             BMS_Util.LowWarning(minValue, maxValue, value, batteryState);
             BMS_Util.HighWarning(minValue, maxValue, value, batteryState);
         }
 
-        private static void CheckBreach(float minValue, float maxValue, float value, string batteryState)
+        private static void Breach(float minValue, float maxValue, float value, string batteryState)
         {
 
             BMS_Util.LowBreach(minValue, maxValue, value, batteryState);

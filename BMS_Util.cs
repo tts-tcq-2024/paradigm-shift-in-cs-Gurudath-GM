d6039c3bd5e4248945eb98cfb003404afcb07bf9
Gurudath G M <princegguruk@gmail.com>
2024-08-07 22:26:29 +0530

    Update BMS_Util.cs

--- BMS_Util.cs
+++ BMS_Util.cs
@@ -4,34 +4,46 @@ namespace paradigm_shift_csharp
     {
         private const float tolerance = 0.05f;
 
-        public static void LowWarning(float minValue, float maxValue, float value, string batteryState)
+        public static bool LowWarning(float minValue, float maxValue, float value, string batteryState)
         {
             if (value > minValue && value <= (minValue + (maxValue * tolerance)))
-                BatteryStatus.Print($"LOW_{batteryState}_WARNING");
+                Print($"LOW_{batteryState}_WARNING");
+            return true;
         }
 
-        public static void HighWarning(float minValue, float maxValue, float value, string batteryState)
+        public static bool HighWarning(float minValue, float maxValue, float value, string batteryState)
         {
             if (value > (maxValue - (maxValue * tolerance)) && value <= maxValue)
-                BatteryStatus.Print($"HIGH_{batteryState}_WARNING");
+                Print($"HIGH_{batteryState}_WARNING");
+            return true;
         }
 
-        public static void LowBreach(float minValue, float maxValue, float value, string batteryState)
+        public static bool LowBreach(float minValue, float maxValue, float value, string batteryState)
         {
             if (value >= (minValue - minValue) && value <= minValue)
             {
-                BatteryStatus.Print($"LOW_{batteryState}_WARNING");
-                BatteryStatus.Print($"LOW_{batteryState}_BREACH");
+                Print($"{batteryState} is out of range!");
+                Print($"LOW_{batteryState}_BREACH");
+                return false;
             }
+            return true;
         }
 
-        public static void HighBreach(float minValue, float maxValue, float value, string batteryState)
+        public static bool HighBreach(float minValue, float maxValue, float value, string batteryState)
         {
             if (value > maxValue && value <= (maxValue + minValue))
             {
-                BatteryStatus.Print($"HIGH_{batteryState}_WARNING");
-                BatteryStatus.Print($"HIGH_{batteryState}_BREACH");
+                Print($"{batteryState} is out of range!");
+                Print($"HIGH_{batteryState}_BREACH");
+                return false;
             }
+            return true;
+        }
+
+        public static void Print(string parameter)
+        {
+            Console.WriteLine(Language.Translator(parameter, Language.selectedLanguage));
+            BatteryManagementSystemTest.printActualCount++;
         }
     }
 }
